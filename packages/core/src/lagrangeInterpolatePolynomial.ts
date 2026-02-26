83b498f5ba57fb66fab689d8cef84f9b58518eaa
hieu-w <maestrokoder@gmail.com>
2026-02-26 17:14:16 +0700

    refactor(core): migrate lagrangeInterpolatePolynomial to bigint/@noble/curves

--- packages/core/src/lagrangeInterpolatePolynomial.ts
+++ packages/core/src/lagrangeInterpolatePolynomial.ts
@@ -1,38 +1,38 @@
-import { EllipticPoint, generatePrivateExcludingIndexes, Point, Polynomial, secp256k1, Share } from "@tkey/common-types";
-import BN from "bn.js";
+import { invert, mod } from "@noble/curves/abstract/modular.js";
+import { bytesToNumberBE } from "@noble/curves/utils.js";
+import { generatePrivateExcludingIndexes, Point, Polynomial, secp256k1, Share } from "@tkey/common-types";
 
 import CoreError from "./errors";
 
-export function generatePrivateBN(): BN {
-  return secp256k1.genKeyPair().getPrivate();
+const N = secp256k1.Point.CURVE().n;
+
+export function generatePrivateBigInt(): bigint {
+  return bytesToNumberBE(secp256k1.utils.randomSecretKey());
 }
 
-const generateEmptyBNArray = (length: number): BN[] => Array.from({ length }, () => new BN(0));
+const generateEmptyBigIntArray = (length: number): bigint[] => Array.from({ length }, () => 0n);
 
-const denominator = (i: number, innerPoints: Array<Point>) => {
-  let result = new BN(1);
+const denominator = (i: number, innerPoints: Point[]) => {
+  let result = 1n;
   const xi = innerPoints[i].x;
   for (let j = innerPoints.length - 1; j >= 0; j -= 1) {
     if (i !== j) {
-      let tmp = new BN(xi);
-      tmp = tmp.sub(innerPoints[j].x);
-      tmp = tmp.umod(secp256k1.curve.n);
-      result = result.mul(tmp);
-      result = result.umod(secp256k1.curve.n);
+      const tmp = mod(xi - innerPoints[j].x, N);
+      result = mod(result * tmp, N);
     }
   }
   return result;
 };
 
-const interpolationPoly = (i: number, innerPoints: Array<Point>): BN[] => {
-  let coefficients = generateEmptyBNArray(innerPoints.length);
+const interpolationPoly = (i: number, innerPoints: Point[]): bigint[] => {
+  let coefficients = generateEmptyBigIntArray(innerPoints.length);
   const d = denominator(i, innerPoints);
-  if (d.cmp(new BN(0)) === 0) {
+  if (d === 0n) {
     throw CoreError.default("Denominator for interpolationPoly is 0");
   }
-  coefficients[0] = d.invm(secp256k1.curve.n);
+  coefficients[0] = invert(d, N);
   for (let k = 0; k < innerPoints.length; k += 1) {
-    const newCoefficients = generateEmptyBNArray(innerPoints.length);
+    const newCoefficients = generateEmptyBigIntArray(innerPoints.length);
     if (k !== i) {
       let j: number;
       if (k < i) {
@@ -42,13 +42,9 @@ const interpolationPoly = (i: number, innerPoints: Array<Point>): BN[] => {
       }
       j -= 1;
       for (; j >= 0; j -= 1) {
-        newCoefficients[j + 1] = newCoefficients[j + 1].add(coefficients[j]);
-        newCoefficients[j + 1] = newCoefficients[j + 1].umod(secp256k1.curve.n);
-        let tmp = new BN(innerPoints[k].x);
-        tmp = tmp.mul(coefficients[j]);
-        tmp = tmp.umod(secp256k1.curve.n);
-        newCoefficients[j] = newCoefficients[j].sub(tmp);
-        newCoefficients[j] = newCoefficients[j].umod(secp256k1.curve.n);
+        newCoefficients[j + 1] = mod(newCoefficients[j + 1] + coefficients[j], N);
+        const tmp = mod(innerPoints[k].x * coefficients[j], N);
+        newCoefficients[j] = mod(newCoefficients[j] - tmp, N);
       }
       coefficients = newCoefficients;
     }
@@ -58,61 +54,53 @@ const interpolationPoly = (i: number, innerPoints: Array<Point>): BN[] => {
 
 const pointSort = (innerPoints: Point[]): Point[] => {
   const pointArrClone = [...innerPoints];
-  pointArrClone.sort((a, b) => a.x.cmp(b.x));
+  pointArrClone.sort((a, b) => (a.x < b.x ? -1 : a.x > b.x ? 1 : 0));
   return pointArrClone;
 };
 
 const lagrange = (unsortedPoints: Point[]) => {
   const sortedPoints = pointSort(unsortedPoints);
-  const polynomial = generateEmptyBNArray(sortedPoints.length);
+  const polynomial = generateEmptyBigIntArray(sortedPoints.length);
   for (let i = 0; i < sortedPoints.length; i += 1) {
     const coefficients = interpolationPoly(i, sortedPoints);
     for (let k = 0; k < sortedPoints.length; k += 1) {
-      let tmp = new BN(sortedPoints[i].y);
-      tmp = tmp.mul(coefficients[k]);
-      polynomial[k] = polynomial[k].add(tmp);
-      polynomial[k] = polynomial[k].umod(secp256k1.curve.n);
+      const tmp = sortedPoints[i].y * coefficients[k];
+      polynomial[k] = mod(polynomial[k] + tmp, N);
     }
   }
   return new Polynomial(polynomial);
 };
 
-export function lagrangeInterpolatePolynomial(points: Array<Point>): Polynomial {
+export function lagrangeInterpolatePolynomial(points: Point[]): Polynomial {
   return lagrange(points);
 }
 
-export function lagrangeInterpolation(shares: BN[], nodeIndex: BN[]): BN {
+export function lagrangeInterpolation(shares: bigint[], nodeIndex: bigint[]): bigint {
   if (shares.length !== nodeIndex.length) {
     throw CoreError.default("shares not equal to nodeIndex length in lagrangeInterpolation");
   }
-  let secret = new BN(0);
+  let secret = 0n;
   for (let i = 0; i < shares.length; i += 1) {
-    let upper = new BN(1);
-    let lower = new BN(1);
+    let upper = 1n;
+    let lower = 1n;
     for (let j = 0; j < shares.length; j += 1) {
       if (i !== j) {
-        upper = upper.mul(nodeIndex[j].neg());
-        upper = upper.umod(secp256k1.curve.n);
-        let temp = nodeIndex[i].sub(nodeIndex[j]);
-        temp = temp.umod(secp256k1.curve.n);
-        lower = lower.mul(temp).umod(secp256k1.curve.n);
+        upper = mod(upper * -nodeIndex[j], N);
+        const temp = mod(nodeIndex[i] - nodeIndex[j], N);
+        lower = mod(lower * temp, N);
       }
     }
-    let delta = upper.mul(lower.invm(secp256k1.curve.n)).umod(secp256k1.curve.n);
-    delta = delta.mul(shares[i]).umod(secp256k1.curve.n);
-    secret = secret.add(delta);
+    let delta = mod(upper * invert(lower, N), N);
+    delta = mod(delta * shares[i], N);
+    secret = secret + delta;
   }
-  return secret.umod(secp256k1.curve.n);
+  return mod(secret, N);
 }
 
-// generateRandomPolynomial - determinisiticShares are assumed random
-export function generateRandomPolynomial(degree: number, secret?: BN, deterministicShares?: Array<Share>): Polynomial {
-  let actualS = secret;
-  if (!secret) {
-    actualS = generatePrivateExcludingIndexes([new BN(0)]);
-  }
+export function generateRandomPolynomial(degree: number, secret?: bigint, deterministicShares?: Share[]): Polynomial {
+  const actualS = secret !== undefined ? secret : generatePrivateExcludingIndexes([0n]);
   if (!deterministicShares) {
-    const poly = [actualS];
+    const poly: bigint[] = [actualS];
     for (let i = 0; i < degree; i += 1) {
       const share = generatePrivateExcludingIndexes(poly);
       poly.push(share);
@@ -128,32 +116,27 @@ export function generateRandomPolynomial(degree: number, secret?: BN, determinis
   }
   const points: Record<string, Point> = {};
   deterministicShares.forEach((share) => {
-    points[share.shareIndex.toString("hex") as string] = new Point(share.shareIndex, share.share);
+    points[share.shareIndex.toString(16)] = new Point(share.shareIndex, share.share);
   });
   for (let i = 0; i < degree - deterministicShares.length; i += 1) {
-    let shareIndex = generatePrivateExcludingIndexes([new BN(0)]);
-    while (points[shareIndex.toString("hex")] !== undefined) {
-      shareIndex = generatePrivateExcludingIndexes([new BN(0)]);
+    let shareIndex = generatePrivateExcludingIndexes([0n]);
+    while (points[shareIndex.toString(16)] !== undefined) {
+      shareIndex = generatePrivateExcludingIndexes([0n]);
     }
-    points[shareIndex.toString("hex")] = new Point(shareIndex, generatePrivateBN());
+    points[shareIndex.toString(16)] = new Point(shareIndex, generatePrivateBigInt());
   }
-  points["0"] = new Point(new BN(0), actualS);
+  points["0"] = new Point(0n, actualS);
   return lagrangeInterpolatePolynomial(Object.values(points));
 }
 
 //  2 + 3x = y | secret for index 1 is 5 >>> g^5 is the commitment | now we have g^2, g^3 and 1, |
-export function polyCommitmentEval(polyCommitments: Array<Point>, index: BN): Point {
-  // convert to base points, this is badly written, its the only way to access the point rn zzz TODO: refactor
-  const basePtPolyCommitments: Array<EllipticPoint> = [];
-  for (let i = 0; i < polyCommitments.length; i += 1) {
-    const key = secp256k1.keyFromPublic({ x: polyCommitments[i].x.toString("hex"), y: polyCommitments[i].y.toString("hex") }, "");
-    basePtPolyCommitments.push(key.getPublic());
-  }
-  let shareCommitment = basePtPolyCommitments[0];
-  for (let i = 1; i < basePtPolyCommitments.length; i += 1) {
-    const factor = index.pow(new BN(i)).umod(secp256k1.n);
-    const e = basePtPolyCommitments[i].mul(factor);
+export function polyCommitmentEval(polyCommitments: Point[], index: bigint): Point {
+  let shareCommitment = polyCommitments[0].toProjectivePoint();
+  for (let i = 1; i < polyCommitments.length; i += 1) {
+    const factor = mod(index ** BigInt(i), N);
+    const e = polyCommitments[i].toProjectivePoint().multiply(factor);
     shareCommitment = shareCommitment.add(e);
   }
-  return new Point(shareCommitment.getX(), shareCommitment.getY());
+  const affine = shareCommitment.toAffine();
+  return new Point(affine.x, affine.y);
 }
