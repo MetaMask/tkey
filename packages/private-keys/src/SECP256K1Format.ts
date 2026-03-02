ffeb23c1f642ceba15eaa59443ba237bf7c4ba18
hieu-w <maestrokoder@gmail.com>
2026-03-02 14:57:02 +0700

    fix: remove randomBytes

--- packages/private-keys/src/SECP256K1Format.ts
+++ packages/private-keys/src/SECP256K1Format.ts
@@ -1,18 +1,6 @@
 import { bytesToNumberBE } from "@noble/curves/utils.js";
 import { generateID, IPrivateKeyFormat, IPrivateKeyStore, secp256k1 } from "@tkey/common-types";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const browserCrypto = global.crypto || (global as any).msCrypto || {};
-
-function randomBytes(size: number): Uint8Array {
-  if (typeof browserCrypto.getRandomValues === "undefined") {
-    return new Uint8Array(browserCrypto.randomBytes(size));
-  }
-  const arr = new Uint8Array(size);
-  browserCrypto.getRandomValues(arr);
-  return arr;
-}
-
 export class SECP256K1Format implements IPrivateKeyFormat {
   privateKey: bigint;
 
@@ -34,7 +22,7 @@ export class SECP256K1Format implements IPrivateKeyFormat {
   createPrivateKeyStore(privateKey?: bigint): IPrivateKeyStore {
     let privKey: bigint;
     if (!privateKey) {
-      privKey = bytesToNumberBE(randomBytes(64));
+      privKey = bytesToNumberBE(secp256k1.utils.randomSecretKey());
     } else {
       if (!this.validatePrivateKey(privateKey)) {
         throw Error("Invalid Private Key");
